3bc4e8eb857157eea2311782735d3cef13b45c18
jufordelo <jugini13@gmail.com>
2024-08-17 08:54:41 -0300

    menuu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -16,24 +16,17 @@
           </li>
             <li class="nav-item">
             <a class="nav-link" href="{{url('personalizado/create')}}"> Personalizados</a>
-          </li>
-            <li class="nav-item">
-            <a class="nav-link" href="{{url('estoque/create')}}"> Estoque</a>
-          </li>
              <li class="nav-item">
             <a class="nav-link" href="{{url('sugestao/create')}}">FeedBacks</a>
           </li>
 
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Área ADM
+              Área ADM | Colaboradores
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="{{url('')}}">Listagem Reserva </a></li>
-              <li><a class="dropdown-item" href="{{url('')}}"> Listagem Pedidos</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="{{url('')}}"> Listagem FeedBacks</a></li>
-
+              <li> <a class="nav-link"  style="color: black;" href="{{url('estoque/create')}}">  Estoque de Produtos </a>
+        
             </ul>
           </li>
 
